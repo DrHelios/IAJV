78d8aa92e96435dfd27a312de275e540cf7e1147
Thefirenight <alexandre.laberche@gmail.com>
2020-09-10 19:37:04 +0200

    example de main

--- IAJV/IAJV.cpp
+++ IAJV/IAJV.cpp
@@ -3,6 +3,7 @@
 
 #include "People.h"
 #include "WorkerTransitions.h"
+#include "Helper.h"
 
 
 // créer classe stateMachine , transition, people
@@ -17,6 +18,12 @@ void CreateStateMachineForPeople(People &p)
 }
 void DestroyStateMachineForPeople(People &p) { p.DestroyStateMachine(); }
 
+GameState game_state;
+std::vector<People> peopleList;
+std::string name;
+float x, y;
+int nb_personne;
+
 int main()
 {	
 	try 
@@ -27,7 +34,49 @@ int main()
 		//for (int i = 0; i < nbOfWorkers)
 		//{
 		//}
-		People p = People();
+		nb_personne = 1;
+		x = 1;
+		y = 1;
+		game_state.beginPos = { 0, 0 };
+		game_state.Stock = 0;
+		game_state.raffinedStock = 0;
+
+		std::cout << "choix quantité max dans pocket" << std::endl;
+		std::cin >> game_state.maxQtInPocket;
+
+		std::cout << "choix quantité max dans de maison à construire" << std::endl;
+		std::cin >> game_state.numberOfHouses;
+
+		std::cout << "choix de la position x de la foret" << std::endl;
+		std::cin >> x;
+		std::cout << "choix de la position y de la foret" << std::endl;
+		std::cin >> y;
+		game_state.forestPos = { x, y };
+
+		std::cout << "choix de la position x de la zone de construction" << std::endl;
+		std::cin >> x;
+		std::cout << "choix de la position y de la zone de construction" << std::endl;
+		std::cin >> y;
+		game_state.buildPos = { x, y };
+
+		std::cout << "choix de la position x du stockage" << std::endl;
+		std::cin >> x;
+		std::cout << "choix de la position y du stockage" << std::endl;
+		std::cin >> y;
+		game_state.stockPos = { x, y };
+
+		std::cout << "choix du nombre de personne" << std::endl;
+		std::cin >> nb_personne;
+
+		for (int i = 0; i < nb_personne;++i)
+		{
+			std::cout << "choix du nom de la personne" << std::endl;
+			std::cin >> name;
+			People p = People(name);
+			peopleList.push_back(p);
+		}
+
+
 
 
 	} 
--- IAJV/People.h
+++ IAJV/People.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "StateMachine.h"
 #include "Position.h"
+#include "Helper.h"
 
 class People
 {
 private:
 	Position pos{ 0.f,0.f };
 	int quantityInPocket;
+	std::string name;
 	StateMachine* state_machine;
 public:
-	People() { quantityInPocket = 0; }
-	People(int pocket) :  quantityInPocket(pocket) { }
+	People() : quantityInPocket(0), name("Paul")  { }
+	People(int pocket) : quantityInPocket(pocket), name("Paul") { }
+	People(std::string param_name) : quantityInPocket(0), name(param_name) { }
+	std::string GetName() const { return name; }
 	Position GetPosition() const { return pos; }
 	int GetQuantityInPocket() const { return quantityInPocket; }
 	StateMachine* GetStateMachine() const { return state_machine; }
